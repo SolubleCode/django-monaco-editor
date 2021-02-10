3073c4def7d456d5fd181428ab8d99abf5b2d129
Collin Mutembei <collinmutembei@users.noreply.github.com>
2021-02-10 15:45:57 +0300

    Remove static_url reference

--- djangomonacoeditor/config.py
+++ djangomonacoeditor/config.py
@@ -3,11 +3,11 @@ from django.conf import settings
 WEB_EDITOR_STATICFILES = {
     "monaco": {
         "js": (
-            f"{settings.STATIC_URL}monaco/vs/loader.js",
-            f"{settings.STATIC_URL}djangomonacoeditor/monaco/monaco.config.js",
+            "monaco/vs/loader.js",
+            "djangomonacoeditor/monaco/monaco.config.js",
         ),
         "css": {
-            "screen": (f"{settings.STATIC_URL}djangomonacoeditor/monaco/monaco.custom.css", )
+            "screen": ("djangomonacoeditor/monaco/monaco.custom.css", )
         }
     }
 }
