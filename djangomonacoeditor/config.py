d87d986c5afa376056aaf441bd493caf92c2bc2a
Collin Mutembei <collinmutembei@users.noreply.github.com>
2021-02-10 15:32:58 +0300

    Update location of loader.js

--- djangomonacoeditor/config.py
+++ djangomonacoeditor/config.py
@@ -3,7 +3,7 @@ from django.conf import settings
 WEB_EDITOR_STATICFILES = {
     "monaco": {
         "js": (
-            f"{settings.STATIC_URL}monaco/loader.js",
+            f"{settings.STATIC_URL}monaco/vs/loader.js",
             f"{settings.STATIC_URL}djangomonacoeditor/monaco/monaco.config.js",
         ),
         "css": {
